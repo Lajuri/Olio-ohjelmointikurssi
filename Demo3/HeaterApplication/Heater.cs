64e1432d06f6182d7514931ff3e00ccb2e1090f1
h3465 <h3465@Student.jamk.fi>
2017-01-26 11:06:43 +0200

    Tuntiteht

---
+++ Demo3/HeaterApplication/Heater.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterApplication
+{
+    class Heater
+    {
+        //properties
+        public bool IsOn { get; set; }
+        public int Temperature { get; set; }
+        public double Humidity { get; set; }
+
+        //construction
+        public Heater()
+        {
+
+        }
+        public Heater(bool isOn, int temperature, double humidity)
+        {
+            IsOn = isOn;
+            Temperature = temperature;
+            Humidity = humidity;
+
+        }
+
+
+        //Method to switch on and off
+        public void Switch()
+        {
+            if (IsOn == true)
+                IsOn = false;
+            else
+                IsOn = true;
+
+        }
+        //Method to adjust temperature
+        public void Temp(int value)
+        {
+            Temperature = value;
+        }
+        //Method to adjust humidity
+        public void Humi(int value)
+        { 
+            Humidity = value;
+        }
+        public void PrintData()
+        {
+            if (IsOn == true)
+                Console.WriteLine("Heater is On");
+            else
+                Console.WriteLine("Heater is Off");
+            Console.WriteLine("Temperature: " + Temperature);
+            Console.WriteLine("Humidity: " + Humidity+"%");
+
+        }
+
+    }
+}
---
+++ Demo3/HeaterApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice=1;
+            int number;
+            Heater harvia = new Heater();
+            harvia.PrintData();
+            while (choice != 0)
+            {
+                Console.WriteLine("Harvia control panel");
+                Console.WriteLine("1. Switch on/off");
+                Console.WriteLine("2. Adjust temperature");
+                Console.WriteLine("3. Adjust humidity");
+                Console.WriteLine("0. exit");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        harvia.Switch();
+                        break;
+                    case 2:
+                        Console.WriteLine("Give new temperature:");
+                        number = int.Parse(Console.ReadLine());
+
+                        break;
+
+
+
+                }
+                harvia.Switch();
+                harvia.Temp(90);
+                harvia.Humi(70);
+                harvia.PrintData();
+            }
+        }
+    }
+}
