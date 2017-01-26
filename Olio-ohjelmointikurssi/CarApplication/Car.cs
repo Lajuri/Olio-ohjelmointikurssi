49760e9fe2c834b6361ed8387493dabb71500d10
h3465 <h3465@Student.jamk.fi>
2017-01-26 10:14:31 +0200

    Tuntiesimerkki

---
+++ Olio-ohjelmointikurssi/CarApplication/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool FuzzyDices { get; set; }
+        public int DoorCount { get; set; }
+        public static int SomeProperty { get; set; }
+
+        //Default constructor
+
+        public Car()
+        {
+
+        }
+
+        //Constructor
+        public Car(string model)
+        {
+            Model = model;
+        }
+
+        //Method to give more speed
+        public void Accelerate()
+        {
+            Speed += 10;
+
+        }
+        //Method to decrease speed
+        public void Brake(int value)
+        {
+            Speed -= value;
+        
+        }
+
+        //method to display car properties
+
+        public void PrintData()
+        {
+            Console.WriteLine("Car data");
+            Console.WriteLine("Model: " + Model);
+            Console.WriteLine("Color: " + Color);
+            Console.WriteLine("Engine: " + Engine);
+            Console.WriteLine("Speed: " + Speed);
+            Console.WriteLine("Dices: " + FuzzyDices);
+            Console.WriteLine("Doors: " + DoorCount);
+            Console.WriteLine("Property: " + SomeProperty);
+        }
+     
+    }
+}
---
+++ Olio-ohjelmointikurssi/CarApplication/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car hiivatti = new Car();
+            hiivatti.PrintData();
+            hiivatti.Model = "Fiat Bravo";
+            hiivatti.Color = "Yellow";
+            hiivatti.Engine = 1.2;
+            hiivatti.Speed = 10;
+            hiivatti.FuzzyDices = true;
+            hiivatti.DoorCount = 3;
+            Car.SomeProperty = 100;
+            hiivatti.PrintData();
+            hiivatti.Accelerate();
+            Console.WriteLine("Speed = " + hiivatti.Speed);
+            hiivatti.Brake(2);
+            Console.WriteLine("Speed = " + hiivatti.Speed);
+
+            //Create a new instance from Car class
+
+            Car nascar = new Car("Speedstrep");
+            nascar.PrintData();
+            nascar.Color = "Black";
+            nascar.Engine = 5.0;
+            nascar.FuzzyDices = true;
+            nascar.DoorCount = 3;
+            nascar.PrintData();
+           
+
+
+        }
+    }
+}
